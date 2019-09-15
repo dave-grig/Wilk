0c1fb0f9d522130715ee1fba43f5fb3e0da9dfd1
David G <davidgrig88@gmail.com>
2019-09-15 16:45:19 +0400

    1. Connected to Bootstrap, 2. Connected to Owl Carousel 2, 3. Images added for Owl Carousel and for the gallery, 4. Added slider to the main page and styled via CSS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        960:{
+            items:3
+        }
+    },
+    dots:false
+})
