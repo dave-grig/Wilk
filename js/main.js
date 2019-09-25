d3d5d8755f050e4bfc670762bcd10feeac38d24f
David G <davidgrig88@gmail.com>
2019-09-25 19:10:28 +0400

    added navigation menu

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $('.owl-carousel').owlCarousel({
     loop:false,
 
     nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     responsive:{
         0:{
             items:1
