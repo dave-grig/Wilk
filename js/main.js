0d9fdd316ca4953ecdffd21b3ba234de9fff9c46
David G <davidgrig88@gmail.com>
2019-11-04 17:26:38 +0400

    Second Owl Carousel added to main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-$('.owl-carousel').owlCarousel({
+$('.owl-one').owlCarousel({
     loop:false,
 
+    nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        960:{
+            items:3
+        }
+    },
+    dots:false
+})
+
+$('.owl-two').owlCarousel({
+    loop:false,
+    margin: 45,
     nav:true,
     navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     responsive:{
